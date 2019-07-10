368fb9c7748baf03b825aa39e233d6dc263c1e4f
Abdelhafid Allou <allou.abdelhafid@gmail.com>
2019-07-10 22:39:41 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,7 @@
+This repo is a poc for using :
+- Spring reactive
+- Spring security
+- Spring oauth2 resource server
+- OIDC (OpenID Connect)
+
+To be continued
---
+++ src/main/java/perso/spring/reactive/springreactiveoauth2/SpringReactiveOauth2Application.java
@@ -0,0 +1,13 @@
+package perso.spring.reactive.springreactiveoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveOauth2Application.class, args);
+	}
+
+}
