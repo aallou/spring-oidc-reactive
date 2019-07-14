93f324f452dc297ec4ba951815ce2060820c8fd9
Abdelhafid Allou <allou.abdelhafid@gmail.com>
2019-07-14 02:38:01 +0200

    Add userinfo

--- web-app-login/src/main/java/perso/spring/reactive/webapplogin/api/HelloController.java
+++ web-app-login/src/main/java/perso/spring/reactive/webapplogin/api/HelloController.java
@@ -1,5 +1,7 @@
 package perso.spring.reactive.webapplogin.api;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -13,4 +15,10 @@ public class HelloController {
     public Mono<String> hello(Principal user) {
         return Mono.just("Hello " + user.getName());
     }
+
+    @GetMapping(value = "/userinfo", produces = {"application/json"})
+    public OAuth2User userinfo(OAuth2AuthenticationToken authentication) {
+        return authentication.getPrincipal();
+    }
+
 }
